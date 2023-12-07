d3f50be451d957e157ba8634099953a0a59fdf88
Esteban Herrera <stv.herrera@gmail.com>
2023-12-07 01:15:36 -0600

    Add more plugins

--- config/.vim/init.lua
+++ config/.vim/init.lua
@@ -63,6 +63,12 @@ packer.startup(function()
     }
 
     ----------------------------------------------------------------------------
+    -- Lua Plugins / Finder / telescope-ui-select.nvim
+    ----------------------------------------------------------------------------
+    -- It sets vim.ui.select to telescope
+    use {'nvim-telescope/telescope-ui-select.nvim' }
+
+    ----------------------------------------------------------------------------
     -- Lua Plugins / Help / which-key.nvim
     ----------------------------------------------------------------------------
     -- It displays a popup with possible keybindings of the command you started
@@ -78,6 +84,15 @@ packer.startup(function()
                 -- refer to the configuration section below
             }
         end
+    }
+
+    ----------------------------------------------------------------------------
+    -- Lua Plugins / IDE / neovim-session-manager
+    ----------------------------------------------------------------------------
+    -- Manage sessions like folders in VSCode
+    use {
+        'Shatur/neovim-session-manager',
+        requires = 'nvim-lua/plenary.nvim',
     }
 
     ----------------------------------------------------------------------------
@@ -299,8 +314,31 @@ telescope.setup {
     --   extension_config_key = value,
     -- }
     -- please take a look at the readme of the extension you want to configure
+    ["ui-select"] = {
+      require("telescope.themes").get_dropdown {
+        -- even more opts
+      }
+
+      -- pseudo code / specification for writing custom displays, like the one
+      -- for "codeactions"
+      -- specific_opts = {
+      --   [kind] = {
+      --     make_indexed = function(items) -> indexed_items, width,
+      --     make_displayer = function(widths) -> displayer
+      --     make_display = function(displayer) -> function(e)
+      --     make_ordinal = function(e) -> string
+      --   },
+      --   -- for example to disable the custom builtin "codeactions" display
+      --      do the following
+      --   codeactions = false,
+      -- }
+    }
   },
 }
+
+-- To get ui-select loaded and working with telescope, you need to call
+-- load_extension, somewhere after setup function.
+require("telescope").load_extension("ui-select")
 
 --------------------------------------------------------------------------------
 -- Lua Plugin options / which-key.nvim
@@ -392,6 +430,28 @@ require("which-key").setup({
     buftypes = {},
     filetypes = {},
   },
+})
+
+--------------------------------------------------------------------------------
+-- Lua Plugins options neovim-session-manager
+--------------------------------------------------------------------------------
+local Path = require('plenary.path')
+local config = require('session_manager.config')
+require('session_manager').setup({
+  sessions_dir = Path:new(vim.fn.stdpath('data'), 'sessions'),  -- The directory where the session files will be saved.
+  session_filename_to_dir = session_filename_to_dir,  -- Function that replaces symbols into separators and colons to transform filename into a session directory.
+  dir_to_session_filename = dir_to_session_filename,  -- Function that replaces separators and colons into special symbols to transform session directory into a filename. Should use `vim.loop.cwd()` if the passed `dir` is `nil`.
+  autoload_mode = config.AutoloadMode.Disabled,       -- Define what to do when Neovim is started without arguments. Possible values: Disabled, CurrentDir, LastSession
+  autosave_last_session = false,                      -- Automatically save last session on exit and on session switch.
+  autosave_ignore_not_normal = true,                  -- Plugin will not save a session when no buffers are opened, or all of them aren't writable or listed.
+  autosave_ignore_dirs = {},                          -- A list of directories where the session will not be autosaved.
+  autosave_ignore_filetypes = {                       -- All buffers of these file types will be closed before the session is saved.
+    'gitcommit',
+    'gitrebase',
+  },
+  autosave_ignore_buftypes = {},     -- All buffers of these bufer types will be closed before the session is saved.
+  autosave_only_in_session = false,  -- Always autosaves session. If true, only autosaves after a session is active.
+  max_path_length = 80,              -- Shorten the display path if length exceeds this threshold. Use 0 if don't want to shorten the path at all.
 })
 
 --------------------------------------------------------------------------------
